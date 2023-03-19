76738b4b71bf2efc26abd1d099e1284f8ee8b839
Islam Sulaiman <ma.eslam@yahoo.com>
2023-03-19 23:25:09 +0200

    firist commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Todo } from './todoType';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+
+
+export class TodoComponent {
+
+  todos: Todo[] = [
+
+  ]
+  // todo name
+  newTodo: string | undefined;
+
+  saveTodo(){
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.title = this.newTodo;
+      todo.completed = false;
+      this.todos.push(todo);
+      this.newTodo = "";
+
+    }else{
+      alert("please enter a todo")
+    }
+  }
+
+
+  fineshed(id:number){
+    this.todos[id].completed = !this.todos[id].completed
+  }
+
+  remove(id:number){
+    this.todos.splice(id,1)
+  }
+
+}
+
